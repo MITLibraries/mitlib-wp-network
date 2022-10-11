6c60c9c6ba91164f85dd6f51090f4adeb292d109
Matt Bernhardt <mjbernha@mit.edu>
2022-10-11 15:34:07 -0400

    Add WP-SCSS fork, and some sparse themes to build
    ** Why are these changes being introduced:
    
    * We need to build out a working system for compiling SASS stylesheets
      within WordPress itself, to confirm that this approach is going to
      work for our parent theme.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/engx-207
    
    ** How does this address that need:
    
    * This adds the build system we intend to use, the WP-SCSS plugin.
      However, we are adding a forked copy which has been modified to work
      under Pantheon. The change has been submitted as a PR to the plugin
      maintainer, so hopefully we'll be able to remove our fork soon-ish.
    * This commit adds a small parent theme, mitlib-parent, which will be
      expanded upon in future commits. It is, like our current parent theme,
      a theme built from scratch.
    * Also included are three child themes of that parent, which are
      exceedingly minimal (only a stylesheet and a screenshot). These will
      be expanded in future tickets, not this one. I include them here only
      to make sure that we don't somehow break the parent/child relationship
      along the way.
    
    ** Document any side effects to this change:
    
    * The forked version of WP-SCSS is currently coming out of my personal
      GitHub. The PR that I submitted is expected by the maintainer, so
      hopefully the review will not take long. However, if it does not
      come quickly, we may want to move the fork under the Libraries' org.
    * This includes a regenerated composer.lock, so some (constrained)
      updates to minor versions may creep in here. Looking at you,
      roave/security-advisories ...

---
+++ web/app/themes/mitlib-parent/functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * @package MITlib_Parent
+ * @since 0.0.1
+ */
+
+namespace Mitlib\Parent;
+
+/**
+ * Ensure that the directories expected by SCSS compiler exist.
+ *
+ * The WP-SCSS plugin that we rely on to compile theme stylesheets requires that
+ * some directories exist within the uploads directory. This function makes
+ * sure that those directories have been created.
+ *
+ * PLEASE NOTE: This function gets called by child themes as well, so those
+ * folders will be created as well.
+ */
+function setup_scss_folders()
+{
+    // Look up information to build local path.
+    $upload_dir = WP_CONTENT_DIR . '/uploads';
+    $theme = get_option('stylesheet');
+
+    // Create the build directory, if needed.
+    $build_dir = $upload_dir . '/wp-scss/' . $theme . '-build';
+    if (!wp_mkdir_p($build_dir)) {
+        error_log('Failed to create build directory: ' . $build_dir);
+    }
+
+    // Create the cache directory, if needed.
+    $cache_dir = $upload_dir . '/wp-scss/' . $theme . '-cache';
+    if (!wp_mkdir_p($cache_dir)) {
+        error_log('Failed to create cache directory: ' . $cache_dir);
+    }
+}
+add_action('after_setup_theme', 'Mitlib\Parent\setup_scss_folders');
