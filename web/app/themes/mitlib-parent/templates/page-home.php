e57353ac72cac2601c6d88d2754db2a2edb20d55
Matt Bernhardt <mjbernha@mit.edu>
2022-10-11 15:34:07 -0400

    Add WP-SCSS fork, and some sparse themes to build
    ** Why are these changes being introduced:
    
    * We need to implement some simple themes (one parent and three
      children), which will be built out into the real themes over the
      coming weeks.
      * The parent theme needs to have some additional templates and
        partials (a 404 page, header and footer partials, and a a few
        options for page templates)
      * The child themes can get away with just a stylesheet and a
        screenshot for now.
    * The parent theme, and the application as a whole, needs to have the
      WP-SCSS plugin installed so that we can use this as a replacement
      for our curret build system.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/engx-207
    
    ** How does this address that need:
    
    * This creates four simple themes - a parent a three children - which
      can be minimally identified and expanded down the road.
    * This adds the build system we intend to use, the WP-SCSS plugin.
      However, we are adding a forked copy which has been modified to work
      under Pantheon. The change has been submitted as a PR to the plugin
      maintainer, so hopefully we'll be able to remove our fork soon-ish.
    
    ** Document any side effects to this change:
    
    * The forked version of WP-SCSS is currently coming out of my personal
      GitHub. The PR that I submitted is expected by the maintainer, so
      hopefully the review will not take long. However, if it does not
      come quickly, we may want to move the fork under the Libraries' org.
    * This includes a regenerated composer.lock, so some (constrained)
      updates to minor versions may creep in here. Looking at you,
      roave/security-advisories ...

---
+++ web/app/themes/mitlib-parent/templates/page-home.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @package MITlib_Parent
+ * @since 0.0.1
+ */
+
+namespace Mitlib\Parent;
+
+get_header();
+?>
+
+<div id="content">
+    <p>This is the home page template.</p>
+</div>
+
+<?php get_footer(); ?>
