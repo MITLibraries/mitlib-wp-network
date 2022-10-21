e4de117a27289b0da418aa9a5d566afb015d7d0d
Matt Bernhardt <mjbernha@mit.edu>
2022-10-21 15:42:09 -0400

    Slight reorganization of script registration to try and simplify things.

--- web/app/themes/mitlib-parent/functions.php
+++ web/app/themes/mitlib-parent/functions.php
@@ -51,6 +51,8 @@ function setup_scripts_styles()
     // increment the theme version here.
     $theme_version = wp_get_theme()->get('Version');
 
+    /* Stylesheets */
+
     // Deal with stylesheets.
     wp_register_style('font-open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300,400,400italic,600,600italic,700,700italic', false, null, 'all');
 
@@ -78,13 +80,13 @@ function setup_scripts_styles()
     wp_enqueue_style('parent-ie', get_template_directory_uri() . '/css/ie.css', array('parent-style'), '20121010');
     wp_style_add_data('parent-ie', 'conditional', 'lt IE 9');
 
-    // Deal with scripts.
+    /* Javascript */
 
-    // Deregister WP Core jQuery, load Google's.
+    // Deregister WP Core jQuery, load our own.
     wp_deregister_script('jquery');
-
     wp_register_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '1.11.1-local', false);
 
+    // Register various other libraries.
     wp_register_script('bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array( 'jquery' ), true); // All the bootstrap javascript goodness.
 
     wp_register_script('jquery.smartmenus', '/js/bootstrap-js/jquery.smartmenus.js', array( 'jquery' ), true); // All the bootstrap javascript goodness.
@@ -99,21 +101,11 @@ function setup_scripts_styles()
 
     wp_register_script('underscore', get_template_directory_uri() . '/js/libs/underscore.js', array(), '1.7.0', true);
 
-    wp_register_script('homeJS', get_template_directory_uri() . '/js/build/home.min.js', array( 'jquery', 'modernizr', 'moment', 'underscore' ), '1.10.0', true);
-
-    wp_register_script('productionJS', get_template_directory_uri() . '/js/build/production.min.js', array( 'jquery', 'moment', 'underscore' ), '1.10.0', true);
-
     wp_register_script('polyfill', '//polyfill.io/v3/polyfill.js?version=3.52.1', array(), '3.52.1', true);
 
     wp_register_script('hours-loader', get_template_directory_uri() . '/js/hours-loader.js', array( 'moment', 'underscore', 'polyfill' ), '1.10.0', true);
 
     wp_register_script('hours-gldatepickerJS', get_template_directory_uri() . '/libs/datepicker/glDatePicker.min.js', false, null, true);
-
-    wp_register_script('hoursJS', get_template_directory_uri() . '/js/build/hours.min.js', array( 'jquery', 'productionJS', 'hours-gldatepickerJS' ), '1.10.0', true);
-
-    wp_register_script('searchJS', get_template_directory_uri() . '/js/build/search.min.js', array( 'jquery', 'modernizr' ), '1.5.5', false);
-
-    wp_register_script('mapJS', get_template_directory_uri() . '/js/build/map.min.js', array( 'jquery' ), '1.5.5', true);
 
     wp_register_script('googleMapsAPI', '//maps.googleapis.com/maps/api/js?key=AIzaSyDJg6fTKm3Pa_NfKEVAdyeRUbVs7zZm5Nw', array(), '1.7.0', true);
 
@@ -121,15 +113,19 @@ function setup_scripts_styles()
 
     wp_register_script('privacyJS', get_template_directory_uri() . '/js/privacy-notice.js', array(), $theme_version, false);
 
+    // Register the five compiled stylesheets.
+    wp_register_script('homeJS', get_template_directory_uri() . '/js/build/home.min.js', array( 'jquery', 'modernizr', 'moment', 'underscore' ), $theme_version, true);
+    wp_register_script('productionJS', get_template_directory_uri() . '/js/build/production.min.js', array( 'jquery', 'moment', 'underscore' ), $theme_version, true);
+    wp_register_script('hoursJS', get_template_directory_uri() . '/js/build/hours.min.js', array( 'jquery', 'productionJS', 'hours-gldatepickerJS' ), $theme_version, true);
+    wp_register_script('searchJS', get_template_directory_uri() . '/js/build/search.min.js', array( 'jquery', 'modernizr' ), $theme_version, false);
+    wp_register_script('mapJS', get_template_directory_uri() . '/js/build/map.min.js', array( 'jquery' ), $theme_version, true);
+
     /* All-site JS */
-
     wp_enqueue_script('hours-loader');
-
     wp_enqueue_script('modernizr');
-
     wp_enqueue_script('privacyJS');
 
-    /* Page-specific JS & CSS */
+    /* Conditionally enqueue JS and CSS based on various criteria */
 
     if (! is_front_page() || is_child_theme()) {
         wp_enqueue_script('productionJS');
