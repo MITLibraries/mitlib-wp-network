44e1904cd72d1da2f8f45bec6a7dc067d2612397
Matt Bernhardt <mjbernha@mit.edu>
2022-11-08 11:42:46 -0500

    Updates to analytics plugin
    ** Why are these changes being introduced:
    
    * While this plugin was already named according to our current standard,
      the move to the monorepo does require some additional changes.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/lm-13
    
    ** How does this address that need:
    
    * The license comment block is removed from the PHP script, and the
      plugin metadata in the header is updated (removing URLs, and making
      the author "MIT Libraries").
    * The Codeclimate badges are also removed from the readme.
    
    ** Document any side effects to this change:
    
    * None

--- web/app/plugins/mitlib-analytics/README.md
+++ web/app/plugins/mitlib-analytics/README.md
@@ -1,8 +1,5 @@
 # MITlib Analytics
 
-[![Code Climate](https://codeclimate.com/github/MITLibraries/mitlib-analytics/badges/gpa.svg)](https://codeclimate.com/github/MITLibraries/mitlib-analytics)
-[![Issue Count](https://codeclimate.com/github/MITLibraries/mitlib-analytics/badges/issue_count.svg)](https://codeclimate.com/github/MITLibraries/mitlib-analytics)
-
 This Wordpress plugin provides a very thin implementation of Google Analytics
 which supports cross-domain implementation with other GA properties. For
 example, the Libraries' public-facing web presence spans both libraries.mit.edu
--- web/app/plugins/mitlib-analytics/mitlib-analytics.php
+++ web/app/plugins/mitlib-analytics/mitlib-analytics.php
@@ -1,31 +1,13 @@
 <?php
 /**
  * Plugin Name: MITlib Analytics
- * Plugin URI: https://github.com/MITLibraries/mitlib-analytics
  * Description: This plugin provides a thin implementation of Google Analytics tracking for use across domains.
  * Version: 1.1.0-beta2
- * Author: Matt Bernhardt for MIT Libraries
- * Author URI: https://github.com/MITLibraries
+ * Author: MIT Libraries
  * License: GPL2
  *
  * @package MITlib Analytics
- * @author Matt Bernhardt
- * @link https://github.com/MITLibraries/mitlib-analytics
- */
-
-/**
- * MITlib Analytics is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * MITlib Analytics is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MITlib Analytics. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+ * @author MIT Libraries
  */
 
 namespace mitlib;
