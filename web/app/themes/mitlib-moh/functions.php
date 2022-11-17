a674432185b1c7ee528f9713a1348264cfbe63cd
Matt Bernhardt <mjbernha@mit.edu>
2022-11-17 15:08:57 -0500

    Update MOH styles
    ** Why are these changes being introduced:
    
    * The MOH theme has some references to now-deleted stylesheetgs, and
      inefficient processes.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/lm-62
    
    ** How does this address that need:
    
    * Converts an @import call in style.css to an explicit dependency in
      functions.php (the 'parent-style' line on L22).
    * Removes references to non-existent 'p3' and 'css/bootstrap.css'
      stylesheets.
    * Removes a duplicated call to a stylesheet that's already loaded
      (the 'twentytwelve-style' stylesheet).
    * Makes the external bootstrap CSS a dependency of the moh-global
      stylesheet, with a single enqueue call.
    * Uses theme version lookups for this and the parent theme for more
      consistent cache busting.
    * Updates SCSS files that are included elsewhere with an underscore
      prefix (homepage, interviews, and tablepress)
    
    ** Document any side effects to this change:
    
    * None here, but it is possible that we may need to go fetch some of
      these non-existent stylesheets (like p3) out of git history for the
      source theme repo down the road. I doubt this, as there are style
      rules that seem to come from that sheet in the _interviews.scss
      partial.

--- web/app/themes/mitlib-moh/functions.php
+++ web/app/themes/mitlib-moh/functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * music-oral-history functions and definitions.
+ * Functions and definitions for the Music Oral History theme.
  *
  * @package Mitlib-moh
  * @since 0.0.1
@@ -8,22 +8,23 @@
 
 namespace Mitlib\Moh;
 
+/**
+ * Register and selectively enqueue the scripts and stylesheets needed for this
+ * page.
+ */
 function moh_scripts_styles() {
+	// This allows us to cache-bust these assets without needing to remember to
+	// increment the theme version here.
+	$theme_version = wp_get_theme()->get( 'Version' );
+	$parent_version = wp_get_theme()->parent()->get( 'Version' );
 
-    /* Register JS & CSS */
-    wp_register_style( 'bootstrapCSS', get_stylesheet_directory_uri() . '/css/bootstrap.css', 'false', '', false );
-    wp_register_style( 'global.css', get_stylesheet_directory_uri().'/css/build/minified/global.css', array( 'libraries-global' ), '20140423' );
+	/* Register JS & CSS */
+	wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), $parent_version );
+	wp_register_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css', array(), '3.0.0' );
+	wp_register_style( 'moh-global', get_stylesheet_directory_uri() . '/css/build/global.css', array( 'parent-style', 'bootstrap' ), $theme_version );
 
-    /* Queue scripts and styles */
-    wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
-    wp_enqueue_style( 'global.css' );
-    wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri(), array(), '20140328' );
-
-    /* Page-specific JS */
-
-    if ( is_page( 'search-all-interviews' ) ) {
-            wp_enqueue_style( 'p3' );
-    }
+	/* Queue scripts and styles */
+	wp_enqueue_style( 'moh-global' );
 }
 
 add_action( 'wp_enqueue_scripts', 'Mitlib\Moh\moh_scripts_styles' );
