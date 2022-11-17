62a80c71cd1034059d2e0f445705501d01cebfaf
Matt Bernhardt <mjbernha@mit.edu>
2022-11-17 16:12:13 -0500

    Updates to news theme styles
    ** Why are these changes being introduced:
    
    * There are some inefficiencies with how the news theme registers and
      loads stylesheets, which should be cleared up.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/lm-62
    
    ** How does this address that need:
    
    * Moves a custom admin stylesheet into the /css directory
    * Extracts some styles that were being written directly by functions.php
      to a stylesheet in the /css directory (hide-addthis.css)
    * Combines stylesheet loading into two functions, one for public-facing
      styles (the wp_enqueue_scripts action) and one for admin interface
      styles (the admin_head action).
    * Of particular note is the previous not_admin function, which had
      confusing logic and code comments. The code comment was not accurate
      to what is_admin() actually checks (it detects whether a page is in
      the admin dashboard or not - so adds nothing for the wp_enqueu_scripts
      action). This tracks with what is in these stylesheets, which only
      make sense for public-facing pages.
    * An @import call to the parent theme's style.css is removed from this
      style.css, and enqueued as it should be by functions.php the
      'parent-style' asset on lines 27 and 30)
    
    ** Document any side effects to this change:
    
    * The actual stylesheets loaded in the admin interface seem non-sensical
      and unneeded, but I'm choosing not to inspect these too closely yet as
      those plugins aren't enabled yet. (This probably isn't a side effect,
      but it may be "a problem we are carrying forward").

--- web/app/themes/mitlib-news/functions.php
+++ web/app/themes/mitlib-news/functions.php
@@ -9,48 +9,47 @@
 namespace Mitlib\News;
 
 /**
- * Add Bootstrap and mobile CSS for non-admin users
+ * Register and selectively enqueue stylesheets needed for the current page.
  */
-function not_admin() {
-	wp_enqueue_style( 'bootstrapCSS', get_stylesheet_directory_uri() . '/css/bootstrap.css', 'false', '', false );
-	wp_enqueue_style( 'newsmobile', get_stylesheet_directory_uri() . '/css/newsmobile.css', 'false', '', false );
-}
-if ( ! is_admin() ) {
-	add_action( 'wp_enqueue_scripts', 'Mitlib\News\not_admin' );
-}
+function news_scripts_styles() {
+	/**
+	 * This allows us to cache-bust these assets without needing to remember to
+	 * increment the theme version here.
+	 */
+	$theme_version = wp_get_theme()->get( 'Version' );
+	$parent_version = wp_get_theme()->parent()->get( 'Version' );
 
-/**
- * Add stylesheets for all users.
- */
-function add_styles() {
 	// Load FontAwesome via CDN.
-	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css' );
+	wp_register_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css', array(), '4.6.0' );
+	wp_enqueue_style( 'font-awesome' );
+
+	// Load local bootstrap and mobile styles.
+	wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), $parent_version );
+	wp_register_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), $theme_version );
+	wp_register_style( 'newsmobile', get_stylesheet_directory_uri() . '/css/newsmobile.css', array(), $theme_version );
+	wp_enqueue_style( 'parent-style' );
+	wp_enqueue_style( 'bootstrap' );
+	wp_enqueue_style( 'newsmobile' );
 }
-add_action( 'wp_enqueue_scripts', 'Mitlib\News\add_styles' );
+add_action( 'wp_enqueue_scripts', 'Mitlib\News\news_scripts_styles' );
 
 /**
- * Hide addthis widget from non-admins on dashboard
+ * Registers custom css files for admin dashboard
  */
-function hide_addthis() {
-	global $user_level;
-	if ( '10' != $user_level ) {
-	   echo '<style type="text/css">
-		   #at_widget,
-		   .metabox-prefs label:nth-child(13) {
-			   display: none;
-			   }
-		 </style>';
+function admin_styles() {
+	/**
+	 * This allows us to cache-bust these assets without needing to remember to
+	 * increment the theme version here.
+	 */
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_register_style( 'custom-admin', get_stylesheet_directory_uri() . '/css/custom-admin.css', array(), $theme_version );
+	wp_enqueue_style( 'custom-admin' );
+
+	// Users who cannot edit theme options (i.e. non-administrators) are not shown the AddThis panel.
+	if ( ! current_user_can( 'edit_theme_options' ) ) {
+		wp_register_style( 'hide-addthis', get_stylesheet_directory_uri() . '/css/hide-addthis.css', array(), $theme_version );
+		wp_enqueue_style( 'hide-addthis' );
 	}
 }
-add_action( 'admin_head', 'hide_addthis' );
-
-/**
- * Registers custom css file for admin dashboard
- */
-function registerCustomAdminCss() {
-$src = '/wp-content/themes/mit-libraries-news/custom-admin-css.css';
-$handle = 'customAdminCss';
-wp_register_script( $handle, $src );
-wp_enqueue_style( $handle, $src, array(), false, false );
-	}
-	add_action( 'admin_head', 'registerCustomAdminCss' );
+add_action( 'admin_head', 'Mitlib\News\admin_styles' );
