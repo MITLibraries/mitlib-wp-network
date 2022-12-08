3c8b84351a45d46ebe060716981fb182cd753592
Matt Bernhardt <mjbernha@mit.edu>
2022-12-08 17:22:03 -0500

    Cleanup MOH theme javascript
    ** Why are these changes being introduced:
    
    * The copy-pasted javascript from the previous commit has some bugs.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/lm-61
    
    ** How does this address that need:
    
    This resolves some incorrect aspects of the javascript setup for this
    theme.
    * Some js libs are not loaded, so can be deleted.
    * A previous - and unused - concatenation of theme javascript is
      somehow still present.
    * PHPCS flagged some flaws in how javascript was being registered
      without version numbers.
    * The bootstrap-js script was being duplicatively defined.
    
    ** Document any side effects to this change:
    
    * None, hopefully

--- web/app/themes/mitlib-moh/functions.php
+++ web/app/themes/mitlib-moh/functions.php
@@ -23,18 +23,17 @@ function moh_scripts_styles() {
 	wp_register_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css', array(), '3.0.0' );
 	wp_register_style( 'moh-global', get_stylesheet_directory_uri() . '/css/build/global.css', array( 'parent-style', 'bootstrap' ), $theme_version );
 
-	wp_register_script( 'easyXDM', get_stylesheet_directory_uri().'/js/libs/easyXDM.min.js', array( 'jquery' ), false, false );
-	wp_register_script( '3play', get_stylesheet_directory_uri().'/js/libs/3playmedia.js', array( 'jquery' ), '3.0', true, true );
-	wp_register_script( '3play-player', get_stylesheet_directory_uri().'/js/libs/3play.player.js', array( 'jquery' ), false, true );
-	wp_register_script( '3play-hack', get_stylesheet_directory_uri().'/js/3play.hack.js', array( 'jquery', '3play', '3play-player' ), false, true );
+	wp_register_script( 'easyXDM', get_stylesheet_directory_uri() . '/js/libs/easyXDM.min.js', array( 'jquery' ), $theme_version, false );
+	wp_register_script( '3play', get_stylesheet_directory_uri() . '/js/libs/3playmedia.js', array( 'jquery' ), '3.0', true, true );
+	wp_register_script( '3play-player', get_stylesheet_directory_uri() . '/js/libs/3play.player.js', array( 'jquery' ), $theme_version, true );
+	wp_register_script( '3play-hack', get_stylesheet_directory_uri() . '/js/3play.hack.js', array( 'jquery', '3play', '3play-player' ), $theme_version, true );
 	wp_register_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array( 'jquery' ), true ); // All the bootstrap javascript goodness.
 
 	/* Queue scripts and styles */
-	wp_enqueue_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array( 'jquery' ), true );
+	wp_enqueue_script( 'bootstrap-js' );
 	wp_enqueue_style( 'moh-global' );
 
 	/* Page-specific JS */
-
 	if ( is_page( 'search-all-interviews' ) ) {
 			wp_enqueue_script( 'easyXDM' );
 			wp_enqueue_script( '3play' );
