0e9f898107186ddcda8ac90238955638aa9c43ad
Matt Bernhardt <mjbernha@mit.edu>
2022-12-09 10:29:35 -0500

    Add child theme javascript
    ** Why are these changes being introduced:
    
    * The child theme, upon closer inspection, does not have any of its own
      javascript (either libraries in folders, or natively-written js).
      However, it does load two libraries from external sources.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/lm-61
    
    ** How does this address that need:
    
    * This adds register / enqueue calls for the external libraries loaded
      by the child theme (jquery and bootstrap).
    
    ** Document any side effects to this change:
    
    * There is a potential side effect coming in terms of how the various
      themes load jquery. I've chosen to start with the descendent themes
      first, and note that this child theme is loading jquery 1.9, while
      the parent theme uses something different. This will probably be a
      future ticket to standardize and modernize this system.

--- web/app/themes/mitlib-child/functions.php
+++ web/app/themes/mitlib-child/functions.php
@@ -16,11 +16,19 @@ namespace Mitlib\Child;
  * compiled global.css).
  */
 function child_scripts_styles() {
-	// First we enqueue style libraries.
+	// First we register stylesheet libraries.
 	wp_register_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css', array(), '3.0.0' );
 	wp_register_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css', array(), '4.6.0' );
 
+	// Then we register javascript libraries.
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', array(), '1.9.1', true );
+	wp_register_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array( 'jquery' ), '3.0.0' );
+
+	// Finally we enqueue those libraries - the child theme just always enqueues everything.
 	wp_enqueue_style( 'bootstrap' );
 	wp_enqueue_style( 'font-awesome' );
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'bootstrap-js' );
 }
 add_action( 'wp_enqueue_scripts', 'Mitlib\Child\child_scripts_styles' );
