1dce25c726f06c59b280bf3f907fb9ecd80d403d
Matt Bernhardt <mjbernha@mit.edu>
2023-01-09 16:05:51 -0500

    Code changes to location-info.php

--- web/app/themes/mitlib-parent/inc/location-info.php
+++ web/app/themes/mitlib-parent/inc/location-info.php
@@ -8,20 +8,20 @@
 
 namespace Mitlib\Parent;
 
-	// Query the locations.
-	$args = array(
-		'post_type' => 'location',
-		'posts_per_page' => 6,
-		'orderby' => 'menu_order',
-		'order' => 'ASC',
-		'no_found_rows' => true,
-		'update_post_term_cache' => false,
-		'update_post_meta_cache' => false,
-	);
+// Query the locations.
+$args = array(
+	'post_type' => 'location',
+	'posts_per_page' => 6,
+	'orderby' => 'menu_order',
+	'order' => 'ASC',
+	'no_found_rows' => true,
+	'update_post_term_cache' => false,
+	'update_post_meta_cache' => false,
+);
 
-	$locationsQuery = new WP_Query( $args );
+$locationsQuery = new WP_Query( $args );
 
-	while ( $locationsQuery->have_posts() ) {
-			$locationsQuery->the_post();
-			echo '<div class="location"><h3>' . get_the_title() . '</h3></div>';
-	}
+while ( $locationsQuery->have_posts() ) {
+	$locationsQuery->the_post();
+	echo '<div class="location"><h3>' . get_the_title() . '</h3></div>';
+}
