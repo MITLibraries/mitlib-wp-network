5c910d5b2f3e8d6c38a30b52c7f13980837f1c66
Matt Bernhardt <mjbernha@mit.edu>
2023-01-09 15:21:32 -0500

    changes to content-root.php

--- web/app/themes/mitlib-parent/inc/content-root.php
+++ web/app/themes/mitlib-parent/inc/content-root.php
@@ -6,16 +6,18 @@
  * @since 0.2.0
  */
 
-$pageRoot = getRoot( $post );
-$section = get_post( $pageRoot );
-$isRoot = $section->ID == $post->ID;
+namespace Mitlib\Parent;
+
+$page_root = get_root( $post );
+$section = get_post( $page_root );
+$is_root = $section->ID == $post->ID;
 
 ?>
 
 <div class="title-page">
-				<?php if ( $isRoot ) : ?>
-				<h1><?php echo $section->post_title; ?></h1>
-				<?php else : ?>
-				<a class="title-page-link" href="<?php echo get_permalink( $section->ID ); ?>"><?php echo $section->post_title; ?></a>
-				<?php endif; ?>
-			</div>
+	<?php if ( $is_root ) : ?>
+		<h1><?php echo esc_html( $section->post_title ); ?></h1>
+	<?php else : ?>
+		<a class="title-page-link" href="<?php the_permalink( $section->ID ); ?>"><?php echo esc_html( $section->post_title ); ?></a>
+	<?php endif; ?>
+</div>
