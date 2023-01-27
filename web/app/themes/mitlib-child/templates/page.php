6f3e64cc4382add7785b5886e06b5cd401e9c90d
Matt Bernhardt <mjbernha@mit.edu>
2023-01-27 13:55:27 -0500

    Update file comment for Standard Child template
    ** Why are these changes being introduced:
    
    * The structure of the Standard Child page template changed after I
      edited the file comment for this template, and now needs to be updated
    
    ** How does this address that need:
    
    * This updates the file comment for the Standard Page template, making
      it clear that this is now the preferred page template for the theme,
      and that it supports both widget areas defined in the theme.
    
    ** Document any side effects to this change:
    
    * None

--- web/app/themes/mitlib-child/templates/page.php
+++ web/app/themes/mitlib-child/templates/page.php
@@ -3,10 +3,15 @@
  * Template Name: Standard Child
  *
  * This page template is an override of the Standard Template which is inherited
- * from the Parent theme.
+ * from the Parent theme. It is the preferred page template for displaying a
+ * Page record, unless a specialized feature is needed like:
+ * - Displaying sticky Posts above the content (such as a site-level alert)
+ * - Showing a paginated set of Posts below the content (such as a Recent News-
+ *   type page for a site)
  *
- * Please note that while this template does load the Main Sidebar defined by
- * the Child theme, it does _not_ load the "Below Content Widget Area" sidebar.
+ * This template supports both widget areas defined by the site, the `sidebar`
+ * that usually shows up in a right column, and `sidebar-two` which is loaded
+ * by inc/content-page below the page content block.
  *
  * @package MITlib_Child
  * @since 0.1.0
