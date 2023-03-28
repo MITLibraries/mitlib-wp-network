35fb0ae85ee069e8879a46f9089b2046f9bcd773
Matt Bernhardt <mjbernha@mit.edu>
2023-03-28 17:28:37 -0400

    Cleaning single.php
    - Namespacing and file metadata
    - Fixing one call to DateTime
    - Escaping a number of lines as prompted by PHPCS

--- web/app/themes/mitlib-news/single.php
+++ web/app/themes/mitlib-news/single.php
@@ -2,9 +2,11 @@
 /**
  * The template for displaying all single posts.
  *
- * @package MITLibraries-News
- * @since Twenty Twelve 1.0
+ * @package MITlib_News
+ * @since 0.2.0
  */
+
+namespace Mitlib\News;
 
 get_header();
 $category = get_the_category();
@@ -29,7 +31,7 @@ if ( ( get_post_type( get_the_ID() ) == 'bibliotech' ) || ( cat_is_ancestor_of( 
 while ( have_posts() ) :
 	the_post();
 	?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-category="<?php echo $category[0]->slug; ?>">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-category="<?php echo esc_attr( $category[0]->slug ); ?>">
 	<div class="title-page  mySingle">     
 	  <?php the_title( '<h1 class="entry-title single">', '</h1>' ); ?>
 	  <?php if ( get_field( 'subtitle' ) ) { ?>
@@ -63,7 +65,7 @@ while ( have_posts() ) :
 
 					$r = rand( 0, $rCat - 1 );
 
-					echo '<a title="' . $category[ $r ]->cat_name . '"  title="' . $category[ $r ]->cat_name . '" href="' . get_category_link( $category[ $r ]->term_id ) . '">' . $category[ $r ]->cat_name . '</a>';
+					echo '<a title="' . esc_attr( $category[ $r ]->cat_name ) . '" href="' . esc_url( get_category_link( $category[ $r ]->term_id ) ) . '">' . esc_html( $category[ $r ]->cat_name ) . '</a>';
 					?>
 		</span>
 		<?php endif; ?>
@@ -80,7 +82,7 @@ while ( have_posts() ) :
 	
 	<?php
 	if ( get_field( 'event_date' ) ) {
-				$date = DateTime::createFromFormat( 'Ymd', get_field( 'event_date' ) );
+				$date = \DateTime::createFromFormat( 'Ymd', get_field( 'event_date' ) );
 
 		?>
 		  <!--EVENT --> 
@@ -104,11 +106,11 @@ while ( have_posts() ) :
 	</g>
 </g>
 </svg></span>    
-		  <span class="event"><?php echo $date->format( 'F j, Y' ); ?></span> 
+		  <span class="event"><?php echo esc_html( $date->format( 'F j, Y' ) ); ?></span> 
 		  <span class="time">
 			<?php
 			if ( get_field( 'event_start_time' ) ) {
-					echo the_field( 'event_start_time' );
+					echo esc_html( the_field( 'event_start_time' ) );
 			}
 			?>
 			<?php
@@ -118,7 +120,7 @@ while ( have_posts() ) :
 			?>
 			<?php
 			if ( get_field( 'event_end_time' ) ) {
-					echo the_field( 'event_end_time' );
+					echo esc_html( the_field( 'event_end_time' ) );
 			}
 			?>
 			</span> 
@@ -136,7 +138,7 @@ while ( have_posts() ) :
 	<!--=================image=================== -->       
 	<?php if ( get_field( 'image' ) ) { ?>
 	 <div class="mySinglePicMobile hidden-md hidden-lg col-xs-12">
-	   <img data-original="<?php echo get_field( 'image' ); ?> "width="100%" alt="<?php the_title(); ?>" class="thumbnail img-responsive"  /> 
+	   <img data-original="<?php echo esc_url( get_field( 'image' ) ); ?> "width="100%" alt="<?php the_title(); ?>" class="thumbnail img-responsive"  /> 
 		<?php if ( get_field( 'caption' ) ) { ?>
 	   <div class="mitCaption"><?php the_field( 'caption' ); ?></div>
 	   <?php } ?>
@@ -146,7 +148,7 @@ while ( have_posts() ) :
 	<!--=================image=================== -->  
 	<?php if ( get_field( 'image' ) ) { ?>         
 	  <div class="mySinglePic hidden-sm hidden-xs">
-	   <img data-original="<?php echo get_field( 'image' ); ?> "width="679" alt="<?php the_title(); ?>" class="thumbnail img-responsive"  /> 
+	   <img data-original="<?php echo esc_url( get_field( 'image' ) ); ?> " width="679" alt="<?php the_title(); ?>" class="thumbnail img-responsive"  /> 
 		<?php if ( get_field( 'caption' ) ) { ?>
 	   <div class="mitCaption"><?php the_field( 'caption' ); ?></div>
 	   <?php } ?>
@@ -163,14 +165,14 @@ while ( have_posts() ) :
 			// Echo type of Feature, if Feature.
 	if ( 'features' === $type_post ) {
 		$type = get_field( 'feature_type' );
-		echo 'The feature type is' . $type;
+		echo 'The feature type is' . esc_html( $type );
 	}
 			// Echo start/end dates, if they exist.
 	if ( 'exhibits' === $type_post || 'updates' === $type_post ) {
 		$date_start = get_field( 'date_start' );
 		$date_end = get_field( 'date_end' );
-		echo '<div>Start date is ' . $date_start . '</div>';
-		echo '<div>End date is ' . $date_end . '</div>';
+		echo '<div>Start date is ' . esc_html( $date_start ) . '</div>';
+		echo '<div>End date is ' . esc_html( $date_end ) . '</div>';
 	}
 	?>
 	</div>
@@ -183,7 +185,7 @@ while ( have_posts() ) :
 	<div class="container">
 
 <div class="row singleMargin">
-	<div class="text-center moreIn"> More in <span class="lowercase"> <?php echo '<a title="' . $category[ $r ]->cat_name . '"  title="' . $category[ $r ]->cat_name . '" href="' . get_category_link( $category[ $r ]->term_id ) . '">' . $category[ $r ]->cat_name . '</a>'; ?></span>
+	<div class="text-center moreIn"> More in <span class="lowercase"> <?php echo '<a title="' . esc_attr( $category[ $r ]->cat_name ) . '" href="' . esc_url( get_category_link( $category[ $r ]->term_id ) ) . '">' . esc_html( $category[ $r ]->cat_name ) . '</a>'; ?></span>
 	</div>
 </div>
 	<?php wp_reset_postdata(); ?>
