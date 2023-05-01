b32faf06cd7246a019471e2baf0e24e34fce0dc3
Matthew Bernhardt <mjbernha@mit.edu>
2023-05-01 17:06:32 -0400

    Adds Mitlib Post extension plugin for AKDC
    ** Why are these changes being introduced:
    
    * The AKDC site on our network has a some custom fields added to its
      Post content type, which allow this content type to function as an
      event.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/lm-147
    
    ** How does this address that need:
    
    * This defines a site-specific extension for the Mitlib Post plugin
      family, which will be enabled only on the AKDC site. This is the same
      approach which we've previously adopted for the Parent and News sites
      when those sites needed to extend the built-in post types.
    
    ** Document any side effects to this change:
    
    * This doubles down on using the Post content type when we might
      really need to use the Events post type - but I'm not going to fight
      that battle right now.

---
+++ web/app/plugins/mitlib-post-site-akdc/mitlib-post-site-akdc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: MITlib Post Site (AKDC site)
+ * Description: Extends the built-in content types for the AKDC site
+ * Version: 1.0.0
+ * Author: MIT Libraries
+ * License: GPL2
+ *
+ * @package MITlib Post Site AKDC
+ * @author MIT Libraries
+ */
+
+namespace Mitlib\PostTypes;
+
+// Don't call the file directly!
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Require the necessary classes.
+require_once( plugin_dir_path( __FILE__ ) . 'src/class-siteakdc.php' );
+
+// Register class methods with the WordPress hooks which will call them.
+add_filter( 'acf/settings/load_json', array( 'Mitlib\PostTypes\SiteAkdc', 'load_point' ) );
+add_filter( 'acf/settings/save_json', array( 'Mitlib\PostTypes\SiteAkdc', 'save_point' ) );
