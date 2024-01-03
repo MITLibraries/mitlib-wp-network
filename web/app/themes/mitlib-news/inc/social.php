a14fc52a1bdd2ed85f7b60e8e5eff1ded87f0c76
Matthew Bernhardt <mjbernha@mit.edu>
2024-01-03 16:37:11 -0500

    Remove social icons from news header; refactor CSS
    ** Why are these changes being introduced:
    
    * We are removing social media icons from across our website. In some
      cases, like the news site header, we are placing a link to a basic
      "Follow us" page that will allow us to more easily manage changes to
      which profiles the Libraries' maintain.
    
    * It turns out that when this area of the news site header is updated,
      some CSS changes are warranted.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/pw-66
    
    ** How does this address that need:
    
    * This replaces the two links to X and Instagram in the News site header
      with a single link named "Follow us".
    
    * The spacing of elements in this list item are updated a bit, in order
      to introduce the pipe element and to ensure proper responsiveness. The
      overall list is no longer floated, but rendered as a flex element.
    
    ** Document any side effects to this change:
    
    * The elements in this list, as well as the site name which appears next
      to it, are now given the "nowrap" property, to prevent some awkward
      displays at some screen widths.
    
    * The News theme version is also incremented, following our practice of
      bumping this version to help with CSS cache busting. It should be
      noted that this stylesheet is not compiled by SCSS, but the cache
      busting is still a good thing regardless.

--- web/app/themes/mitlib-news/inc/social.php
+++ web/app/themes/mitlib-news/inc/social.php
@@ -10,25 +10,16 @@ namespace Mitlib\News;
 
 ?>
 
-<ul class="sIcons hidden-xs anotherMenu clearfix pull-right">
+<ul class="sIcons hidden-xs anotherMenu clearfix pull-right flex-container">
 	<li class="sub soc">
 		<a title="subscribe" href="/news/subscribe/" >
 		Subscribe to news
 		</a>
-		</li>
-		<li class="tw soc">
-		<a title="twitter"  href="//twitter.com/mitlibraries" class="twitterLink">
-		<span class="si_twitter image_on"></span>
-		<span class="si_twitterBlue image_off"></span>
-		</a>
-		</li>
-		<li class="ig soc">
-		<a title="Instagram"  href="//instagram.com/mitlibraries/">
-		<span class="si_instagram image_on"></span>
-		<span class="si_instagramBlue image_off"></span>
-		</a>
-		</li>
-		 <li class="sf">
+	</li>
+	<li class="follow soc">
+		<a title="follow" href="/follow">Follow us</a>
+	</li>
+	<li class="sf">
 		<?php get_search_form( 'true' ); ?>
-			</li>
-		</ul>
+	</li>
+</ul>
