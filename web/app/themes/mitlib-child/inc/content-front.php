eb495af99dd2a8b5e19eae0c4bd19a61e6ea9719
djanelle-mit <djanelle@mit.edu>
2025-09-24 15:07:54 -0400

    Update from aria-label to title attribute for button

--- web/app/themes/mitlib-child/inc/content-front.php
+++ web/app/themes/mitlib-child/inc/content-front.php
@@ -43,7 +43,7 @@ if ( count( $sticky ) > 0 ) {
 			<div>				
 				<h3><?php the_title(); ?></h3>
 				<?php custom_excerpt( 20, '...' ); ?>
-				<a class="btn btn-secondary" aria-label="Read more about <?php the_title(); ?>" href="<?php echo esc_url( the_permalink() ); ?>">Read more</a>			
+				<a class="btn btn-secondary" title="Read more about <?php the_title(); ?>" href="<?php echo esc_url( the_permalink() ); ?>">Read more</a>			
 			</div>			
 		</aside>
 
