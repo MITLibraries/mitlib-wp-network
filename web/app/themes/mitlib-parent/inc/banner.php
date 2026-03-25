d6ccf55c2ba6423075eddd20b7f7813a200a5c8b
djanelle-mit <djanelle@mit.edu>
2026-03-25 13:35:00 -0400

    Adding temporary USE callout banner to library homepage

---
+++ web/app/themes/mitlib-parent/inc/banner.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template for USE Homepage Banner
+ *
+ * @package MITlib_Parent
+ * @since 0.2.0
+ */
+
+namespace Mitlib\Parent;
+
+?>
+<div class="mitlib-banner info">
+	<div class="wrap-notice">
+		<h1 class="title"><a href="https://search.libraries.mit.edu">Try our new search</a> before it launches this summer!</div></h1>
+	</div>
+</div>
