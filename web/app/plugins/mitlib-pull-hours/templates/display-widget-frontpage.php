9a6d2dd0ddbf738cce498e699b90def6d698e30f
djanelle-mit <djanelle@mit.edu>
2026-03-26 14:54:45 -0400

    Added ACF field for unstaffed locations to location attributes and updated UI when checked

--- web/app/plugins/mitlib-pull-hours/templates/display-widget-frontpage.php
+++ web/app/plugins/mitlib-pull-hours/templates/display-widget-frontpage.php
@@ -11,17 +11,17 @@
 <div class="location">
 	<a href="/barker" aria-labelledby="barker" class="img-loc barker"><span class="sr" id="barker">Barker Library</span></a>
 	<div class="wrap-loc-info">
-		<h3><a class="name-location" href="/barker">Barker Library</a></h3><div class="hours"><span data-location-hours="Barker Library"></span> today</div><div class="location-info"><a href="/locations/#!barker-library" class="map-location">10-500</a></div>
+		<h3><a class="name-location" href="/barker">Barker Library</a></h3><div class="hours"><span data-location-hours="Barker Library"></span> today</div><div class="location-info"><a href="/locations/#!barker-library" class="map-location">10-500</a><?php if ( get_field("unstaffed_location", 322) == false ) : ?><a href="tel:617-253-0968" class="phone"><span class="number">617-253-0968</span></a><?php endif; ?></div>
 	</div>
 </div>
-<?php
+<?php	
 	// Load location alerts for Barker (post 322).
 	$this->location_alert( 322 );
 ?>
 <div class="location">
 	<a href="/dewey" aria-labelledby="dewey" class="img-loc dewey"><span class="sr" id="dewey">Dewey Library</span></a>
 	<div class="wrap-loc-info">
-		<h3><a class="name-location" href="/dewey">Dewey Library</a></h3><div class="hours"><span data-location-hours="Dewey Library"></span> today</div><div class="location-info"><a href="/locations/#!dewey-library" class="map-location">E53-100</a><a href="tel:617-253-5676" class="phone"><span class="number">617-253-5676</span></a></div>
+		<h3><a class="name-location" href="/dewey">Dewey Library</a></h3><div class="hours"><span data-location-hours="Dewey Library"></span> today</div><div class="location-info"><a href="/locations/#!dewey-library" class="map-location">E53-100</a><?php if ( get_field("unstaffed_location", 313) == false ) : ?><a href="tel:617-253-5676" class="phone"><span class="number">617-253-5676</span></a><?php endif; ?></div>
 	</div>
 </div>
 <?php
@@ -31,7 +31,7 @@
 <div class="location">
 	<a href="/hayden" aria-labelledby="hayden" class="img-loc hayden"><span class="sr" id="hayden">Hayden Library</span></a>
 	<div class="wrap-loc-info">
-		<h3><a class="name-location" href="/hayden">Hayden Library</a></h3><div class="hours"><span data-location-hours="Hayden Library"></span> today</div> <div class="location-info"><a href="/locations/#!hayden-library" class="map-location">14S-100</a><a href="tel:617-253-5671" class="phone"><span class="number">617-253-5671</span></a></div>
+		<h3><a class="name-location" href="/hayden">Hayden Library</a></h3><div class="hours"><span data-location-hours="Hayden Library"></span> today</div> <div class="location-info"><a href="/locations/#!hayden-library" class="map-location">14S-100</a><?php if ( get_field("unstaffed_location", 452) == false ) : ?><a href="tel:617-253-5671" class="phone"><span class="number">617-253-5671</span></a><?php endif; ?></div>
 	</div>
 </div>
 <?php
@@ -44,7 +44,7 @@
 <div class="location hidden-mobile inactive-mobile">
 	<a href="/rotch" aria-labelledby="rotch" class="img-loc rotch"><span class="sr" id="rotch">Rotch Library</span></a>
 	<div class="wrap-loc-info">
-		<h3><a class="name-location" href="/rotch">Rotch Library</a></h3><div class="hours"><span data-location-hours="Rotch Library"></span> today</div><div class="location-info"><a href="/locations/#!rotch-library" class="map-location">7-238</a><a href="tel:617-258-5592" class="phone"><span class="number">617-258-5592</span></a></div>
+		<h3><a class="name-location" href="/rotch">Rotch Library</a></h3><div class="hours"><span data-location-hours="Rotch Library"></span> today</div><div class="location-info"><a href="/locations/#!rotch-library" class="map-location">7-238</a><?php if ( get_field("unstaffed_location", 359) == false ) : ?><a href="tel:617-258-5592" class="phone"><span class="number">617-258-5592</span></a><?php endif; ?></div>
 	</div>
 </div>
 <?php
@@ -54,7 +54,7 @@
 <div class="location hidden-mobile inactive-mobile">
 	<a href="/distinctive-collections" aria-labelledby="dc" class="img-loc archives"><span class="sr" id="dc">Distinctive Collections Reading Room</span></a>
 	<div class="wrap-loc-info">
-		<h3><a class="name-location" href="/distinctive-collections">Distinctive Collections Reading Room</a></h3><div class="hours"><span data-location-hours="Distinctive Collections"></span> today</div><div class="location-info"><a href="/locations/#!distinctive-collections" class="map-location">14N-118</a><a href="tel:617-253-5690" class="phone"><span class="number">617-253-5690</span></a></div>
+		<h3><a class="name-location" href="/distinctive-collections">Distinctive Collections Reading Room</a></h3><div class="hours"><span data-location-hours="Distinctive Collections"></span> today</div><div class="location-info"><a href="/locations/#!distinctive-collections" class="map-location">14N-118</a><?php if ( get_field("unstaffed_location", 504) == false ) : ?><a href="tel:617-253-5690" class="phone"><span class="number">617-253-5690</span></a><?php endif; ?></div>
 	</div>
 </div>
 <?php
@@ -64,7 +64,7 @@
 <div class="location hidden-mobile inactive-mobile">
 	<a href="/music" aria-labelledby="lewis" class="img-loc lewis"><span class="sr" id="lewis">Lewis Music Library</span></a>
 	<div class="wrap-loc-info">
-		<h3><a class="name-location" href="/music">Lewis Music Library</a></h3><div class="hours"><span data-location-hours="Lewis Music Library"></span> today</div><div class="location-info"><a href="/locations/#!lewis-music-library" class="map-location">14E-109</a><a href="tel:617-253-5689" class="phone"><span class="number">617-253-5689</span></a></div>
+		<h3><a class="name-location" href="/music">Lewis Music Library</a></h3><div class="hours"><span data-location-hours="Lewis Music Library"></span> today</div><div class="location-info"><a href="/locations/#!lewis-music-library" class="map-location">14E-109</a><?php if ( get_field("unstaffed_location", 473) == false ) : ?><a href="tel:617-253-5689" class="phone"><span class="number">617-253-5689</span></a><?php endif; ?></div>
 	</div>
 </div>
 <?php
