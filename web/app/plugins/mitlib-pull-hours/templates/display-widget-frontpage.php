213965798454b2e487cbb54f558b0b5cffcd0bb6
djanelle-mit <djanelle@mit.edu>
2026-06-15 11:55:16 -0400

    Updated hours reference to old dewey library

--- web/app/plugins/mitlib-pull-hours/templates/display-widget-frontpage.php
+++ web/app/plugins/mitlib-pull-hours/templates/display-widget-frontpage.php
@@ -64,7 +64,7 @@
 <div class="location">
 	<a href="/dewey" aria-labelledby="dewey" class="img-loc dewey"><span class="sr" id="dewey">E53 Study Space</span></a>
 	<div class="wrap-loc-info">
-		<h3><a class="name-location" href="/dewey">E53 Study Space</a></h3><div class="hours"><span data-location-hours="Dewey Library"></span> today</div><div class="location-info"><a href="/locations/#!dewey-library" class="map-location">E53-100</a><?php if ( get_field("unstaffed_location", 313) == false ) : ?><a href="tel:617-253-5676" class="phone"><span class="number">617-253-5676</span></a><?php endif; ?></div>
+		<h3><a class="name-location" href="/dewey">E53 Study Space</a></h3><div class="hours"><span data-location-hours="E53 Study Space"></span> today</div><div class="location-info"><a href="/locations/#!dewey-library" class="map-location">E53-100</a><?php if ( get_field("unstaffed_location", 313) == false ) : ?><a href="tel:617-253-5676" class="phone"><span class="number">617-253-5676</span></a><?php endif; ?></div>
 	</div>
 </div>
 <?php
