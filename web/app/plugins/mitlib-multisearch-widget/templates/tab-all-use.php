b240aecfe718a1524faad0fd784ec2698e5a2335
djanelle-mit <djanelle@mit.edu>
2026-07-21 15:46:18 -0400

    Second part of refactoring search widget

--- web/app/plugins/mitlib-multisearch-widget/templates/tab-all-use.php
+++ web/app/plugins/mitlib-multisearch-widget/templates/tab-all-use.php
@@ -8,27 +8,34 @@
 
 ?>
 <!-- nls state: _<?php echo esc_attr( $nls_enabled ); ?>_ -->
-
-
-<form id="search-form" action="https://search.libraries.mit.edu/results" method="get" role="search">
-	<label for="basic-search-main">What can we help you find?</label>
-	<div class="form-wrapper">
-		<div class="search-input-wrapper">
-			<i class="fa-regular fa-magnifying-glass"></i>
-			<input id="basic-search-main" type="search" class="field field-text basic-search-input" name="q" title="Keyword anywhere" value="" required="">
-			<button title="Clear search" aria-label="Clear search" type="button" id="clear-search" style="display: none;">Clear search</button>
+<form
+	class="form search-bento"
+	action="https://search.libraries.mit.edu/results"
+	method="get"
+	data-target="bento">
+	<label for="searchinput-bento">Search the MIT Libraries</label>
+	<div class="wrap-flex">
+		<div class="flex-left">
+			<input
+				class="field field-text"
+				type="text"
+				id="searchinput-bento"
+				name="q"
+				placeholder="Search across collections, services, and website content">
 		</div>
-		<input id="tab-to-target" type="hidden" name="tab" value="all">
-		<button type="submit" class="btn button-primary">Search</button>
-	</div>
-	<div class="search-actions">
-		<a class="link-on-dark" href="https://libraries.mit.edu/search-advanced">Advanced search</a>			
-
-		<?php if ( 'included' == $nls_included ) { ?>
-			<div class="nls-toggle">
-				<a class="toggle <?php echo esc_attr( $nls_enabled ); ?>" href="<?php echo esc_url( 'https://search.libraries.mit.edu/natural_language_search_optin?natural_language_search_optin=' . $nls_link_toggle . '&return_to=/' ); ?>">Natural language search</a>
-				<a class="learn-more" href="https://search.libraries.mit.edu/about-natural-language-search">Learn more</a>
-			</div>
-		<?php } ?>			
+		<div class="flex-right">
+			<input class="button button-search" type="submit" value="Search">
+		</div>
 	</div>
 </form>
+<div class="search-option-links">
+	<div>
+		<a href="/search-advanced/">Advanced search</a> | <a href="/search/">More ways to search</a>
+	</div>
+	<?php if ( 'included' == $nls_included ) { ?>
+		<div class="nls-toggle">
+			<a class="toggle <?php echo esc_attr( $nls_enabled ); ?>" href="<?php echo esc_url( 'https://search.libraries.mit.edu/natural_language_search_optin?natural_language_search_optin=' . $nls_link_toggle . '&return_to=/' ); ?>">Natural language search</a>
+			<a class="learn-more" href="https://search.libraries.mit.edu/about-natural-language-search">Learn more</a>
+		</div>
+	<?php } ?>
+</div>
