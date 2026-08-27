32aa63e3e9e4ca2ff3cab5111586f4717e8cc4d4
djanelle-mit <djanelle@mit.edu>
2026-08-27 14:07:53 -0400

    Tweaks to hero block and scaffold for hours

--- web/app/plugins/mitlib-block-hero-section/src/mitlib-block-hero-section/render.php
+++ web/app/plugins/mitlib-block-hero-section/src/mitlib-block-hero-section/render.php
@@ -6,8 +6,7 @@
  * @var string   $content    Block default content.
  * @var WP_Block $block      Block instance.
  */
-?>
-<section id="hero" role="img" aria-label="Two notebooks opened to show yellow graph paper; the top one has a black and white photo of a boat crew, and the bottom one shows handwritten text." style="background-image: url(https://libraries.mit.edu/app/uploads/2026/07/hero-image-edgerton.png);">
+?><section id="hero" role="img" aria-label="Two notebooks opened to show yellow graph paper; the top one has a black and white photo of a boat crew, and the bottom one shows handwritten text." style="background-image: url(https://libraries.mit.edu/app/uploads/2026/07/hero-image-edgerton.png);">
 	<div class="overlay">
 		<div class="content-wrapper">
 			<div class="hero-content">
---
+++ web/app/plugins/mitlib-block-hours-section/build/mitlib-block-hours-section/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4ae39ed62ecdf93ef833');
