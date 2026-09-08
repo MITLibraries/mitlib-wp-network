f57fd929887a5c574c04fa47fd1ed2f892df09e6
Matthew Bernhardt <mjbernha@mit.edu>
2026-09-08 17:02:35 -0400

    npm run format

--- web/app/plugins/mitlib-blocks/blocks/hours-section/edit.js
+++ web/app/plugins/mitlib-blocks/blocks/hours-section/edit.js
@@ -4,7 +4,11 @@
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText, InspectorControls } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	InspectorControls,
+} from '@wordpress/block-editor';
 import { PanelBody, TextControl } from '@wordpress/components';
 import './editor.scss';
 
@@ -13,32 +17,38 @@ export default function Edit( { attributes, setAttributes } ) {
 
 	return (
 		<>
-		<InspectorControls>
-			<PanelBody title={ __( 'Link Settings', 'mitlib-blocks' ) }>
-				<TextControl
-					label={ __( 'Link text', 'mitlib-blocks' ) }
-					value={ linkText }
-					onChange={ ( value ) => setAttributes( { linkText: value } ) }
-				/>
-				<TextControl
-					label={ __( 'Link URL', 'mitlib-blocks' ) }
-					value={ linkUrl }
-					onChange={ ( value ) => setAttributes( { linkUrl: value } ) }
-					type="url"
-				/>
-			</PanelBody>
-		</InspectorControls>
-		<section { ...useBlockProps() } id="todays-hours">
-			<div className="content-wrapper">
-				<RichText
-					tagName="h2"
-					value={ heading }
-					onChange={ ( value ) => setAttributes( { heading: value } ) }
-					placeholder={ __( 'Today\'s hours', 'mitlib-blocks' ) }
-					allowedFormats={ [] }
-				/>
-			</div>
-		</section>
+			<InspectorControls>
+				<PanelBody title={ __( 'Link Settings', 'mitlib-blocks' ) }>
+					<TextControl
+						label={ __( 'Link text', 'mitlib-blocks' ) }
+						value={ linkText }
+						onChange={ ( value ) =>
+							setAttributes( { linkText: value } )
+						}
+					/>
+					<TextControl
+						label={ __( 'Link URL', 'mitlib-blocks' ) }
+						value={ linkUrl }
+						onChange={ ( value ) =>
+							setAttributes( { linkUrl: value } )
+						}
+						type="url"
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<section { ...useBlockProps() } id="todays-hours">
+				<div className="content-wrapper">
+					<RichText
+						tagName="h2"
+						value={ heading }
+						onChange={ ( value ) =>
+							setAttributes( { heading: value } )
+						}
+						placeholder={ __( "Today's hours", 'mitlib-blocks' ) }
+						allowedFormats={ [] }
+					/>
+				</div>
+			</section>
 		</>
 	);
 }
--- web/app/plugins/mitlib-blocks/blocks/using-the-libraries-section/save.js
+++ web/app/plugins/mitlib-blocks/blocks/using-the-libraries-section/save.js
@@ -7,45 +7,96 @@
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 export default function save( { attributes } ) {
-	const { heading, askUsTitle, askUsDescription, askUsLinkText, askUsLinkUrl } = attributes;
+	const {
+		heading,
+		askUsTitle,
+		askUsDescription,
+		askUsLinkText,
+		askUsLinkUrl,
+	} = attributes;
 
 	return (
 		<section id="using-the-libraries">
 			<div class="content-wrapper">
 				<RichText.Content tagName="h2" value={ heading } />
 				<div class="box-wrapper">
-				<div class="option-boxes">
-					<div>
-						<i class="fa-light fa-lightbulb" aria-hidden="true" role="img"></i>
-						<div class="option-box-content">
-							<h3><a href="/study">Find a study space</a></h3>
-							<p>Quiet and group spaces—many available 24/7</p>
+					<div class="option-boxes">
+						<div>
+							<i
+								class="fa-light fa-lightbulb"
+								aria-hidden="true"
+								role="img"
+							></i>
+							<div class="option-box-content">
+								<h3>
+									<a href="/study">Find a study space</a>
+								</h3>
+								<p>
+									Quiet and group spaces—many available 24/7
+								</p>
+							</div>
+						</div>
+						<div>
+							<i
+								class="fa-light fa-file-alt"
+								aria-hidden="true"
+								role="img"
+							></i>
+							<div class="option-box-content">
+								<h3>
+									<a href="/get-materials">
+										Learn how to get materials
+									</a>
+								</h3>
+								<p>
+									Find, request, and get articles, books, and
+									more
+								</p>
+							</div>
+						</div>
+						<div>
+							<i
+								class="fa-light fa-book"
+								aria-hidden="true"
+								role="img"
+							></i>
+							<div class="option-box-content">
+								<h3>
+									<a href="/experts">
+										Discover guides &amp; librarians
+									</a>
+								</h3>
+								<p>
+									Resource and class guides with experts for
+									every subject
+								</p>
+							</div>
+						</div>
+						<div>
+							<i
+								class="fa-light fa-database"
+								aria-hidden="true"
+								role="img"
+							></i>
+							<div class="option-box-content">
+								<h3>
+									<a href="/data-services">
+										Find and manage data
+									</a>
+								</h3>
+								<p>
+									Get support from creating and visualizing to
+									using and sharing data
+								</p>
+							</div>
 						</div>
 					</div>
-					<div>
-						<i class="fa-light fa-file-alt" aria-hidden="true" role="img"></i>
-						<div class="option-box-content">
-							<h3><a href="/get-materials">Learn how to get materials</a></h3>
-							<p>Find, request, and get articles, books, and more</p>
-						</div>
-					</div>
-					<div>
-						<i class="fa-light fa-book" aria-hidden="true" role="img"></i>
-						<div class="option-box-content">
-							<h3><a href="/experts">Discover guides &amp; librarians</a></h3>
-							<p>Resource and class guides with experts for every subject</p>
-						</div>
-					</div>
-					<div>
-						<i class="fa-light fa-database" aria-hidden="true" role="img"></i>
-						<div class="option-box-content">
-							<h3><a href="/data-services">Find and manage data</a></h3>
-							<p>Get support from creating and visualizing to using and sharing data</p>
-						</div>
-					</div>												
-				</div>
-				<div class="ask-us-box">
-						<i class="fa-light fa-messages-question" aria-hidden="true" role="img"></i>
+					<div class="ask-us-box">
+						<i
+							class="fa-light fa-messages-question"
+							aria-hidden="true"
+							role="img"
+						></i>
 						<div class="option-box-content">
 							<h3>{ askUsTitle }</h3>
 							<p>{ askUsDescription }</p>
