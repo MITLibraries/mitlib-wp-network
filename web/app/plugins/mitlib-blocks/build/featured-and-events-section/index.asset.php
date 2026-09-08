833c95f23dc309037ef5350d62e2a5af2bc34bdb
Matthew Bernhardt <mjbernha@mit.edu>
2026-09-08 14:58:38 -0400

    Re-run build
    This also removes the deprecated build materials from the hours plugin

--- web/app/plugins/mitlib-blocks/build/featured-and-events-section/index.asset.php
+++ web/app/plugins/mitlib-blocks/build/featured-and-events-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bb1a5cce0fa31fb9ee7f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'adae8abbc4a7ca67c4b9');
--- web/app/plugins/mitlib-blocks/build/using-the-libraries-section/index.asset.php
+++ web/app/plugins/mitlib-blocks/build/using-the-libraries-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'aee81dc16bb1f37e7a4a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '944bc254e6eab98eeafe');
