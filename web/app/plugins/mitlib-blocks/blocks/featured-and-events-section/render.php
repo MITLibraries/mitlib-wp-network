de0a1027054d5c3dd151a6e03335ba322931c6dd
djanelle-mit <djanelle@mit.edu>
2026-09-09 15:29:11 -0400

    First draft select a featured librarian

--- web/app/plugins/mitlib-blocks/blocks/featured-and-events-section/render.php
+++ web/app/plugins/mitlib-blocks/blocks/featured-and-events-section/render.php
@@ -6,6 +6,24 @@
  * @var string   $content    Block default content.
  * @var WP_Block $block      Block instance.
  */
+
+// Look up the librarian chosen in the block editor's "Featured Librarian" panel.
+$featured_expert = null;
+if ( ! empty( $attributes['featuredExpertId'] ) ) {
+	$maybe_expert = get_post( $attributes['featuredExpertId'] );
+	if ( $maybe_expert && 'experts' === $maybe_expert->post_type && 'publish' === $maybe_expert->post_status ) {
+		$featured_expert = $maybe_expert;
+	}
+}
+
+// Fallback used when no librarian has been selected in the block editor.
+$default_expert = array(
+	'name'       => 'Alejandro Paz',
+	'first_name' => 'Alejandro',
+	'url'        => 'https://libguides.mit.edu/profiles/apaz',
+	'image'      => 'https://libapps.s3.amazonaws.com/accounts/349/images/apaz-100x100.jpg',
+	'excerpt'    => 'Librarian for Energy and Environment',
+);
 ?><section id="featured-and-events">
 	<div class="content-wrapper">
 		<div class="featured-content">
@@ -21,17 +39,44 @@
 						</hgroup>
 					</div>
 				</article>
+				<?php
+				if ( $featured_expert ) {
+					$expert_name       = get_the_title( $featured_expert );
+					$expert_first_name = strtok( $expert_name, ' ' );
+					$expert_url        = get_post_meta( $featured_expert->ID, 'expert_url', true );
+					$expert_image      = get_the_post_thumbnail_url( $featured_expert, 'thumbnail' );
+					$expert_excerpt    = get_the_excerpt( $featured_expert );
+				} else {
+					$expert_name       = $default_expert['name'];
+					$expert_first_name = $default_expert['first_name'];
+					$expert_url        = $default_expert['url'];
+					$expert_image      = $default_expert['image'];
+					$expert_excerpt    = $default_expert['excerpt'];
+				}
+				?>
 				<article class="featured-item side-by-side">
 					<span class="item-type spotlight">Spotlight</span>
-					<img src="https://libapps.s3.amazonaws.com/accounts/349/images/apaz-100x100.jpg" alt="Headshot of Alejandro Paz" />
+					<?php if ( $expert_image ) : ?>
+					<img src="<?php echo esc_url( $expert_image ); ?>" alt="<?php echo esc_attr( sprintf( /* translators: %s: librarian name */ __( 'Headshot of %s', 'mitlib-blocks' ), $expert_name ) ); ?>" />
+					<?php endif; ?>
 					<div class="featured-item-content">
 						<hgroup>
-							<h3><a href="https://libguides.mit.edu/profiles/apaz">Alejandro Paz</a></h3>
+							<h3><a href="<?php echo esc_url( $expert_url ); ?>"><?php echo esc_html( $expert_name ); ?></a></h3>
 							<div>
-								<p>Librarian for Energy and Environment</p>
+								<p><?php echo esc_html( $expert_excerpt ); ?></p>
 							</div>
 						</hgroup>
-						<a class="arrow-right" href="https://libguides.mit.edu/profiles/apaz">How can Alejandro help you?</a>
+						<a class="arrow-right" href="<?php echo esc_url( $expert_url ); ?>">
+							<?php
+							echo esc_html(
+								sprintf(
+									/* translators: %s: librarian's first name */
+									__( 'How can %s help you?', 'mitlib-blocks' ),
+									$expert_first_name
+								)
+							);
+							?>
+						</a>
 					</div>
 				</article>
 				<article class="featured-item side-by-side">
--- web/app/plugins/mitlib-blocks/build/featured-and-events-section/index.asset.php
+++ web/app/plugins/mitlib-blocks/build/featured-and-events-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'adae8abbc4a7ca67c4b9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '5535245262429d5d7ae1');
