8dfab47d9a6198ed5ac418a635ea850121994760
djanelle-mit <djanelle@mit.edu>
2026-09-24 15:31:26 -0400

    Moved hero image plugin from mitlib-blocks with no other changes

---
+++ web/app/plugins/mitlib-post-hero-images/build/hero-section/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '860257bfdd4635908f8f');
--- web/app/plugins/mitlib-post-hero-images/mitlib-post-hero-images.php
+++ web/app/plugins/mitlib-post-hero-images/mitlib-post-hero-images.php
@@ -25,3 +25,19 @@ add_action( 'init', array( 'Mitlib\PostTypes\HeroImage', 'define' ) );
 add_action( 'rest_api_init', array( 'Mitlib\PostTypes\HeroImage', 'api_enable' ) );
 add_filter( 'acf/settings/load_json', array( 'Mitlib\PostTypes\HeroImage', 'load_point' ) );
 add_filter( 'acf/settings/save_json', array( 'Mitlib\PostTypes\HeroImage', 'save_point' ) );
+
+/**
+ * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
+ * based on the registered block metadata. Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
+ * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
+ */
+function register_blocks() {
+	wp_register_block_types_from_metadata_collection(
+		__DIR__ . '/build',
+		__DIR__ . '/build/blocks-manifest.php'
+	);
+}
+add_action( 'init', 'Mitlib\PostTypes\register_blocks' );
